279fd7bdfd610c7ecbc6743c211636106229f081
takahiro-itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-12 19:50:10 +0900

    Initialize Refact/0008-Run-CMake

---
+++ View/ViewVb/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub RunCount(ByVal message As String)
+        Dim result As Integer
+        Dim objWrapper As SampleWrapper.Common.SampleDocument = New SampleWrapper.Common.SampleDocument()
+
+        objWrapper.setMessage(message)
+        result = objWrapper.countAlphabet()
+        MsgBox("入力した文字列中のアルファベットの個数は " & result, MsgBoxStyle.OkOnly)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RunCount(TextBox1.Text)
+    End Sub
+
+    Private Sub CountToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CountToolStripMenuItem.Click
+        RunCount(TextBox1.Text)
+    End Sub
+End Class
---
+++ View/Wrapper/Wrapper.cpp
@@ -0,0 +1,70 @@
+﻿// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include <msclr/marshal_cppstd.h>
+
+#include "Wrapper.h"
+
+#include <string>
+
+namespace  SampleWrapper  {
+namespace  Common  {
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+SampleDocument::SampleDocument()
+    : m_ptrObj { new WrapTarget() }
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+SampleDocument::~SampleDocument()
+{
+    this->!SampleDocument();
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （ファイナライザ）。
+//
+
+SampleDocument::!SampleDocument()
+{
+    if ( this->m_ptrObj ) {
+        delete  this->m_ptrObj;
+        this->m_ptrObj  = nullptr;
+    }
+}
+
+//----------------------------------------------------------------
+//    入力メッセージ中に含まれるアルファベットを数える。
+//
+
+int
+SampleDocument::countAlphabet()
+{
+    return ( this->m_ptrObj->countAlphabet() );
+}
+
+//----------------------------------------------------------------
+//    メッセージを設定する。
+//
+
+void
+SampleDocument::setMessage(
+        System::String^ message)
+{
+    std::string tmp = msclr::interop::marshal_as<std::string>(message);
+    this->m_ptrObj->setMessage(tmp);
+}
+
+}   //  End of namespace  Common
+}   //  End of namespace  SampleWrapper
