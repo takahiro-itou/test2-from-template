e55f6ddd95f3f05e38de65723ede82668dceb62c
takahiro-itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-12 19:50:06 +0900

    Initial commit

---
+++ Core/Bin/ViewSample.cpp
@@ -0,0 +1,40 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  DocView Template Project  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2024, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      ビューサンプルプログラム。
+**
+**      @file       Bin/ViewSample.cpp
+**/
+
+#include    "Sample/Common/SampleDocument.h"
+
+#include    <iostream>
+
+using   namespace   SAMPLE_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    Sample::Common::SampleDocument  doc;
+
+    std::string text("Hello, World!");
+
+    doc.setMessage(text);
+    std::cout   <<  "Text : "   <<  text
+                <<  "\nThe number of alphabets in text:"
+                <<  doc.countAlphabet()
+                <<  std::endl;
+
+    return ( 0 );
+}
---
+++ Core/Include/Sample/Common/SampleDocument.h
@@ -0,0 +1,144 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  DocView Template Project  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of SampleDocument class.
+**
+**      @file       Common/SampleDocument.h
+**/
+
+#if !defined( SAMPLE_CONFIG_INCLUDED_SAMPLE_DOCUMENT_H )
+#    define   SAMPLE_CONFIG_INCLUDED_SAMPLE_DOCUMENT_H
+
+#include    "SampleSettings.h"
+
+#include    <string>
+
+SAMPLE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    SampleDocument  class.
+//
+
+class  SampleDocument
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    SampleDocument();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~SampleDocument();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   入力メッセージ中に含まれるアルファベットを数える。
+    **
+    **  @return     半角アルファベット [A-Za-z] の文字数
+    **/
+    virtual  int
+    countAlphabet()  const;
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   メッセージを設定する。
+    **
+    **  @param [in] message   入力データ
+    **  @return     void.
+    **/
+    void
+    setMessage(
+            const  std::string  &message);
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+private:
+
+    std::string     m_message;
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   SampleDocumentTest;
+};
+
+}   //  End of namespace  Common
+SAMPLE_NAMESPACE_END
+
+#endif
