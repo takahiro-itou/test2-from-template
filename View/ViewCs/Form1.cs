645544ee44542c69e2e45a9ff0cf321620c5f5b4
takahiro-itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-12 19:50:11 +0900

    Initialize Refact/0010-UpdateAutoTools

---
+++ View/ViewCs/Form1.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ViewCs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            System.String  message = TextBox1.Text;
+            int result;
+            SampleWrapper.Common.SampleDocument objWrapper = new SampleWrapper.Common.SampleDocument();
+
+            objWrapper.setMessage(message);
+            result = objWrapper.countAlphabet();
+            MessageBox.Show("文字列中のアルファベットの個数は " + result);
+        }
+    }
+
+}
